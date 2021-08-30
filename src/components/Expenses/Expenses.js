775bc37aa1b894005a9097a60a6bed9bca5dcff3
L1ndseyHerman <lherman21@jcu.edu>
2021-08-30 15:52:45 -0400

    Caught up to vid67.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,46 +1,35 @@
 import React, {useState} from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
 
+    //  Want to start off with just the 2020 items tho :( The results are 1 year behind the chosen year.
+    //const [filteredItems, setFilteredItems] = useState(props.items);
+    //  Now that filteredExpenses are a variable instead of a useState(), it works! :D
     const [filteredYear, setFilteredYear] = useState('2020');
 
     const filterChangeHandler = selectedYear => {
+        //setFilteredItems(props.items.filter(filteredExpense => filteredExpense.date.getFullYear().toString() === filteredYear));
         setFilteredYear(selectedYear);
-        //  Darn, still the previous selection and not the current one :(
       };
+
+      const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+      });
 
     return (
         <div>
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            <ExpenseItem 
-                title={props.items[0].title} 
-                amount={props.items[0].amount} 
-                date={props.items[0].date}
-            />
-            <ExpenseItem
-                title={props.items[1].title} 
-                amount={props.items[1].amount} 
-                date={props.items[1].date}
-            />
-            <ExpenseItem
-                title={props.items[2].title} 
-                amount={props.items[2].amount} 
-                date={props.items[2].date}
-            />
-            <ExpenseItem
-                title={props.items[3].title} 
-                amount={props.items[3].amount} 
-                date={props.items[3].date}
-            />
+            <ExpensesList items={filteredExpenses} />
         </Card>
         </div>
     );
+
 }
 
 export default Expenses;
